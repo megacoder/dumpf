094ab039c00cecfe31207bba2a552f39c492f75a
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2015-04-10 14:20:23 -0500

    Redirect stdout and stderr to bootstrap.log file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # vim: nonu ts=8 sw=8
-autoreconf -fvim
-./configure
-make dist
-rm -rf RPM
-mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
-markdown2 README.md | tee README.html | lynx -dump >README
-rpmbuild								\
-	-D"_topdir ${PWD}/RPM"						\
-	-D"_sourcedir ${PWD}"						\
-	-D"_specdir ${PWD}"						\
-	-ba								\
-	dumpf.spec
+(
+	exec 2>&1
+	autoreconf -fvim
+	./configure
+	make dist
+	rm -rf RPM
+	mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+	markdown2 README.md | tee README.html | lynx -dump >README
+	rpmbuild							\
+		-D"_topdir ${PWD}/RPM"					\
+		-D"_sourcedir ${PWD}"					\
+		-D"_specdir ${PWD}"					\
+		-ba							\
+		dumpf.spec
+) | tee bootstrap.log
