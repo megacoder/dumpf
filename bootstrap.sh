f0e2867f6e0035f513f6c00729bca596e384f853
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2015-04-10 14:17:08 -0500

    Don't include generated files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+# vim: nonu ts=8 sw=8
 autoreconf -fvim
 ./configure
 make dist
 rm -rf RPM
 mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+markdown2 README.md | tee README.html | lynx -dump >README
 rpmbuild								\
 	-D"_topdir ${PWD}/RPM"						\
 	-D"_sourcedir ${PWD}"						\
